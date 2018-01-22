df54b77ddf58fda8b5e403e78e3891eef22024ed
abaretta <abaretta@protonmail.com>
2018-01-22 12:30:11 +0100

    update due to API change

--- drivers/winddelen/driver.js
+++ drivers/winddelen/driver.js
@@ -1,7 +1,7 @@
 "use strict";
 
-var http = require("http.min");
-//var parseXML = require("xml2js").parseString;
+var http = require("http");
+var httpmin = require("http.min");
 var et = require('elementtree');
 var XML = et.XML;
 var ElementTree = et.ElementTree;
@@ -9,25 +9,22 @@ var Element = et.Element;
 var SubElement = et.SubElement;
 
 var devices = {};
-//var windmillId = 1;
-var baseurl1 = "http://backend.windcentrale.nl/windcentrale/productie_"
-//_{id}.txt"
+var baseurl1 = "http://backend.windcentrale.nl/windcentrale/productie"
 var baseurl2 = "https://zep-api.windcentrale.nl/production/"
 
-module.exports.init = function(devices_data, callback) {
-    devices_data.forEach(function(device_data) {
+module.exports.init = function (devices_data, callback) {
+    devices_data.forEach(function (device_data) {
         initDevice(device_data, false);
     });
-
     callback(null, true);
 };
 
-module.exports.pair = function(socket) {
-    socket.on("validate", function(data, callback) {
+module.exports.pair = function (socket) {
+    socket.on("validate", function (data, callback) {
         Homey.log("Winddelen app - ID: " + data.id + " number winddelen: " + data.numberofwinddelen);
-        var url = baseurl1 + data.id + ".txt";
+        var url = baseurl1 + "?id=" + device_data.id;
 
-        http.get(url).then(function(result) {
+        http.get(url).then(function (result) {
             if (result.response.statusCode == 200 || result.response.statusCode == 304) {
                 callback(null, true);
             } else {
@@ -37,23 +34,23 @@ module.exports.pair = function(socket) {
     });
 };
 
-module.exports.added = function(device_data, callback) {
+module.exports.added = function (device_data, callback) {
     Homey.log("Winddelen app - [" + device_data.name + "] Added");
 
     initDevice(device_data, true);
     callback(null, true);
 };
 
-module.exports.deleted = function(device_data, callback) {
+module.exports.deleted = function (device_data, callback) {
     Homey.log("Winddelen app - [" + device_data.name + "] Deleted");
 
-    Homey.manager("cron").unregisterTask(devices[device_data.id].cron_name, function(err, success) {});
+    Homey.manager("cron").unregisterTask(devices[device_data.id].cron_name, function (err, success) {});
 
     delete devices[device_data.id];
     callback(null, true);
 };
 
-module.exports.renamed = function(device_data, new_name) {
+module.exports.renamed = function (device_data, new_name) {
     Homey.log("Winddelen app - [" + device_data.name + "] Renamed to: " + new_name);
 
     devices[device_data.id].name = new_name;
@@ -61,7 +58,7 @@ module.exports.renamed = function(device_data, new_name) {
 
 module.exports.capabilities = {
     measure_power: {
-        get: function(device_data, callback) {
+        get: function (device_data, callback) {
             var device = devices[device_data.id];
 
             if (device === undefined) {
@@ -72,7 +69,7 @@ module.exports.capabilities = {
         }
     },
     measure_wind: {
-        get: function(device_data, callback) {
+        get: function (device_data, callback) {
             var device = devices[device_data.id];
 
             if (device === undefined) {
@@ -83,7 +80,7 @@ module.exports.capabilities = {
         }
     },
     meter_power: {
-        get: function(device_data, callback) {
+        get: function (device_data, callback) {
             var device = devices[device_data.id];
 
             if (device === undefined) {
@@ -94,7 +91,7 @@ module.exports.capabilities = {
         }
     },
     meter_year: {
-        get: function(device_data, callback) {
+        get: function (device_data, callback) {
             var device = devices[device_data.id];
 
             if (device === undefined) {
@@ -105,7 +102,7 @@ module.exports.capabilities = {
         }
     },
     meter_lifetime: {
-        get: function(device_data, callback) {
+        get: function (device_data, callback) {
             var device = devices[device_data.id];
 
             if (device === undefined) {
@@ -130,14 +127,14 @@ function initDevice(device_data, cron) {
     };
 
     if (cron) {
-        Homey.manager("cron").registerTask(device.cron_name, "*/5 * * * *", device_data, function(err, task) {
+        Homey.manager("cron").registerTask(device.cron_name, "*/5 * * * *", device_data, function (err, task) {
             if (err !== null) {
                 Homey.log("Winddelen app - [" + device.name + "] Error while creating cron job: " + err);
             }
         });
     }
 
-    Homey.manager("cron").on(device.cron_name, function(device_data) {
+    Homey.manager("cron").on(device.cron_name, function (device_data) {
         checkProduction(device_data);
     });
 }
@@ -147,47 +144,51 @@ function checkProduction(device_data) {
     var numberofwinddelen = device_data.numberofwinddelen;
 
     // fetch current power and wind speed and direction
-    var url1 = baseurl1 + device_data.id + ".txt";
+    var url1 = baseurl1 + "?id=" + device_data.id;
     var url2 = baseurl2 + device_data.id;
 
-    http.get(url1).then(function(result) {
-        if (result.response.statusCode == 200) {
+    http.get(url1, (result) => {
+
+        if (result.statusCode == 200) {
             module.exports.setAvailable(device_data);
+            result.on('data', (body) => {
+                var response = body.toString().split(",");
+                var last_output = response[2];
 
-            var response = result.data.split(",");
-            var last_output = response[2];
+                if (last_output != device.last_output) {
+                    device.last_output = last_output;
+                    // sample output:
+                    // ZO 5,1455,147,62,66,19.4,0,1031797,1609.0833333333,N 4,1489318632,71764
+                    // response[0] -> wind direction and force (Beaufort)
+                    // response[1] -> current total power for windmill 
+                    // response[2] -> current power per share 
+                    // response[3] -> % of max. power 
+                    // response[4] -> 66, ?
+                    // response[5] -> 19.4, temp?
+                    // response[6] -> 0, ?
+                    // response[7] -> total lifetime energy (kWh),
+                    // response[8] -> 1609.0833, ?
+                    // response[9] -> N 4, ?
+                    // response[10] -> 1489318632,?
+                    // response[11] -> 71764 ?
 
-            if (last_output != device.last_output) {
-                device.last_output = last_output;
-                // sample output:
-                // ZO 5,1455,147,62,66,19.4,0,1031797,1609.0833333333,N 4,1489318632,71764
-                // response[0] -> wind direction and force (Beaufort)
-                // response[1] -> current total power for windmill 
-                // response[2] -> current power per share 
-                // response[3] -> % of max. power 
-                // response[4] -> 66, ?
-                // response[5] -> 19.4, temp?
-                // response[6] -> 0, ?
-                // response[7] -> total lifetime energy (kWh),
-                // response[8] -> 1609.0833, ?
-                // response[9] -> N 4, ?
-                // response[10] -> 1489318632,?
-                // response[11] -> 71764 ?
+                    var power = Number(response[2]) * numberofwinddelen;
+                    device.power = power;
+                    module.exports.realtime(device_data, "measure_power", power);
 
-                var power = Number(response[2]) * numberofwinddelen;
-                device.power = power;
-                module.exports.realtime(device_data, "measure_power", power);
+                    var wind = response[0];
+                    device.wind = wind;
+                    module.exports.realtime(device_data, "measure_wind", wind);
 
-                var wind = response[0];
-                device.wind = wind;
-                module.exports.realtime(device_data, "measure_wind", wind);
-
-                //   Homey.log("Winddelen app - [" + device_data.name + "] Energy: " + energy + "kWh");
-                Homey.log("Winddelen app - [" + device_data.name + "] Power: " + power + "W");
-                Homey.log("Winddelen app - [" + device_data.name + "] Wind: " + wind + " Bft");
-            } else {
-                Homey.log("Winddelen app - [" + device_data.name + "] No new data");
-            }
+                    //   Homey.log("Winddelen app - [" + device_data.name + "] Energy: " + energy + "kWh");
+                    Homey.log("Winddelen app - [" + device_data.name + "] Power: " + power + "W");
+                    Homey.log("Winddelen app - [" + device_data.name + "] Wind: " + wind + " Bft");
+                } else {
+                    Homey.log("Winddelen app - [" + device_data.name + "] No new data");
+                }
+            });
+            // the winddelen backend will continue outputting data for minutes, only need 1 line 
+            result.destroy();
         } else {
             Homey.log("Winddelen app - [" + device_data.name + "] Unavailable: " + result.response.statusCode + " error");
 
@@ -195,15 +196,15 @@ function checkProduction(device_data) {
         }
     });
 
-    http.get(url2).then(function(result) {
+    httpmin.get(url2).then(function (result) {
         if (result.response.statusCode == 200) {
             module.exports.setAvailable(device_data);
 
             var etree = et.parse(result.data);
             var winddelenpermill = etree.findall('./productie')[0].get('winddelen');
             var energy = Number(etree.findall('./productie/subset')[0].get('sum')) / winddelenpermill * numberofwinddelen;
-            var yieldYearly = Number(etree.findall('./productie/subset/[@interval="MONTH"]')[0].get('sum')) / winddelenpermill * numberofwinddelen ;
-            var yieldLifetime = Number(etree.findall('./productie/subset/[@interval="YEAR"]')[0].get('sum')) / winddelenpermill * numberofwinddelen ;
+            var yieldYearly = Number(etree.findall('./productie/subset/[@interval="MONTH"]')[0].get('sum')) / winddelenpermill * numberofwinddelen;
+            var yieldLifetime = Number(etree.findall('./productie/subset/[@interval="YEAR"]')[0].get('sum')) / winddelenpermill * numberofwinddelen;
 
             device.energy = energy;
             module.exports.realtime(device_data, "meter_power", device.energy);
